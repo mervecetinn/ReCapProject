879e3c4e48433306a7ef4d48fd62c4560bb3e3b5
mervecetinn <mervecetin3936@gmail.com>
2021-02-17 06:32:35 +0300

    Core layer added.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,9 +10,44 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //CarTest();
+            //BrandTest();
+            //ColorTest();
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetCarDetails())
+            {
+                Console.WriteLine("{0}-{1}-{2}",car.CarName,car.BrandName,car.ColorName);
+            }
+        }
+
+        private static void ColorTest()
+        {
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+            colorManager.Add(new Color { ColorId = 11, ColorName = "Yellow" });
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorId + color.ColorName);
+
+            }
+        }
+
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+            brandManager.Add(new Brand { BrandId = 7, BrandName = "Porsche" });
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandId + brand.BrandName);
+
+            }
+            Console.WriteLine(brandManager.GetById(1).BrandName);
+        }
+
+        private static void CarTest()
+        {
             CarManager carManager = new CarManager(new EfCarDal());
 
-            carManager.Add(new Car {Name="K",BrandId=1,DailyPrice=100,ColorId=2,ModelYear=2004 });
+            carManager.Add(new Car { Name = "K", BrandId = 1, DailyPrice = 100, ColorId = 2, ModelYear = 2004 });
 
             foreach (var car in carManager.GetAll())
             {
